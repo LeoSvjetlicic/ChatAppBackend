31bc320971669bfe55ab0d950487ddfbc181a930
Leo Svjetlicic <leo.svjetlicic@gmail.com>
2024-04-15 15:17:38 +0200

    Add app

--- index.js
+++ index.js
@@ -1,31 +1,54 @@
+import express, { response } from 'express';
+import bodyParser from 'body-parser';
+import admin from 'firebase-admin'; // Importing firebase-admin using default import
+import dotenv from 'dotenv';
+dotenv.config();
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  type:process.env.type,
+  project_id:process.env.project_id,
+  private_key_id:process.env.private_key_id,
+  private_key:process.env.private_key,
+  client_email:process.env.client_email,
+  client_id:process.env.client_id,
+  auth_uri:process.env.auth_uri,
+  token_uri:process.env.token_uri,
+  auth_provider_x509_cert_url:process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url:process.env.client_x509_cert_url,
+  universe_domain:process.env.universe_domain,
+};
 
-import express from 'express'
-import bodyparser from 'body-parser'
-import admin from "./config.js"
-const app = express()
-app.use(bodyparser.json())
+// Initialize Firebase
+admin.initializeApp({ // Using admin.initializeApp instead of initializeAdminApp
+  credential: admin.credential.cert(firebaseConfig)
+});
 
-const port = 8080
-const notification_options = {
-    priority: "high",
-    timeToLive: 60 * 60 * 24
-  };
-  app.post('/firebase/notification', (req, res) => {
-    const registrationToken = req.body.to;
-    const message = req.body.notification;
-    const options = notification_options;
-    console.log("BODY", req.body)
-      admin.messaging().sendToDevice(registrationToken, message, options)
-      .then(response => {
-        console.log("ebo sma  mu mamu");
-        res.status(200).send("Notification sent successfully");
-      })
-      .catch(error => {
-        console.log(req)
-        console.log("ERROR",error);
-        res.status(500).send("Error sending notification");
-      });
-  });
-app.listen(port,"localhost", () =>{
-console.log("listening to port"+port)
-})
+const app = express();
+app.use(bodyParser.json());
+
+const port = 8080;
+const notificationOptions = {
+  priority: 'high',
+  timeToLive: 60 * 60 * 24
+};
+
+app.post('/firebase/notification', (req, res) => {
+  const registrationToken = req.body.to;
+  const message = req.body.notification;
+  const options = notificationOptions;
+
+  admin.messaging().sendToDevice(registrationToken, message, options)
+    .then(response => {
+      console.log('Notification sent successfully');
+      res.status(200).send('Notification sent successfully');
+    })
+    .catch(error => {
+      console.log(response);
+      console.log(error);
+      res.status(500).send('Error sending notification');
+    });
+});
+
+app.listen(port, 'localhost', () => {
+  console.log('Listening to port ' + port);
+});
